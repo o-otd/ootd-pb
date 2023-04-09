f0fe26acfa28dfc165f795ee6c6d0326c57b5b1c
d5ng <dong5ffice@gmail.com>
2023-04-09 16:59:28 +0900

    상세 페이지 레이아웃 완료

--- assets/js/main/activity/category.js
+++ assets/js/main/activity/category.js
@@ -1,4 +1,9 @@
-const swiperCategory = new Swiper(".category-swiper", {
+const categorySwiper = new Swiper(".suggestion-category__swiper", {
+	slidesPerView: 'auto',
+	spaceBetween: 4,
+});
+
+const lnbSwiper = new Swiper(".suggestion-lnb-swiper", {
 	slidesPerView: 'auto',
 	spaceBetween: 4,
 });
