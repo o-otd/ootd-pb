c0137d5520f3472e19a0b38f1452c0505f6f38da
d5ng <dong5ffice@gmail.com>
2023-07-10 18:27:33 +0900

    CalenderJS Update

--- assets/js/confirm/calender.js
+++ assets/js/confirm/calender.js
@@ -13,90 +13,119 @@ const monthObject = {
 	12: 'Dec'
 }
 
-
 class Calender {
-	#currentYear;
-	#currentMonth;
-	#rootElem;
-	#date;
+	#rootElem
+	#date
 
 	constructor({ rootElem, date }){
-		this.#rootElem = rootElem;
-		this.#date = date;
-		this.#currentMonth = date.getMonth() + 1
-		this.#currentYear = date.getFullYear();
+		this.#rootElem = rootElem
+		this.#date = date
 
-		this.render(date);
-		this.eventHandler();
+		this.CalenderUI = new CalenderDraw(this.#date);
+		this.CalenderEvent = new CalenderEvent({
+      date: this.#date,
+      rootElem: this.#rootElem,
+			ui: this.CalenderUI
+    });
 	}
+}
+
+
+class CalenderDraw {
+  currentMonth;
+  currentYear;
+	rootElem;
+	date;
+
+  constructor(date) {
+    this.currentMonth = date.getMonth() + 1;
+    this.currentYear = date.getFullYear();
+		this.date = date
+
+    this.render(this.date);
+  }
 
 	transformMonth(month){
 		return monthObject[month]
 	}
 
-	datePrint(date){
-		console.log(date)
+  datePrint(date) {
+    let dateHTML = "";
 
-		let dateHTML = '';
+		const newDate = new Date(date)
+    const adjDate = new Date(newDate).getDate();
+    const year = new Date(newDate).getFullYear();
+    const month = new Date(newDate).getMonth() + 1;
+    const firstDay = new Date(newDate.setDate(1)).getDay();
+    const lastDay = new Date(year, month, 0).getDate();
+    const limitDay = firstDay + lastDay;
+    const nextDay = Math.ceil(limitDay / 7) * 7;
 
-		const adjDate = new Date(date).getDate();
-		const year = new Date(date).getFullYear();
-  	const month = new Date(date).getMonth() + 1;	
-		const firstDay = new Date(date.setDate(1)).getDay();
-  	const lastDay = new Date(year, month, 0).getDate();
-		const limitDay = firstDay + lastDay;
-		const nextDay = Math.ceil(limitDay / 7) * 7;
+    for (let i = 0; i < firstDay; i++) {
+      dateHTML += `<li class="calender-empty"></li>`;
+    }
 
-		for (let i = 0; i < firstDay; i++) {
-			dateHTML += `<li class="calender-empty"></li>`;
-		}
-	
-		for (let i = 1; i <= lastDay; i++) {
-			if(this.#currentMonth > month) dateHTML += `<li><button disabled>${i}</button></li>`;
-			if(this.#currentMonth <= month && this.#currentYear >= year){
-				const isDisabled = adjDate > i ? "disabled" : ''
-				dateHTML += `<li class="calender-body__dayList"><button ${isDisabled}>${i}</button></li>`;
-			}
+    for (let i = 1; i <= lastDay; i++) {
+			if (this.currentMonth < month) dateHTML += `<li><button>${i}</button></li>`;
+      if (this.currentMonth > month ) dateHTML += `<li><button disabled>${i}</button></li>`;
+      if (this.currentMonth === month) {
+        const isDisabled = adjDate > i ? "disabled" : "";
+        dateHTML += `<li class="calender-body__dayList"><button ${isDisabled}>${i}</button></li>`;
+      }
+    }
 
-		}
-	
-		for (let i = limitDay; i < nextDay; i++) {
-			dateHTML += `<li class="calender-empty"></li>`;
-		}
+    for (let i = limitDay; i < nextDay; i++) {
+      dateHTML += `<li class="calender-empty"></li>`;
+    }
 
-		return { dateHTML, month, year }
+
+    return { dateHTML, month, year };
+  }
+
+  render(date) {
+    const { dateHTML, month, year } = this.datePrint(date);
+    document.querySelector(`.calender-body__date`).innerHTML = dateHTML;
+    document.querySelector(
+      `.calender-header__year span`
+    ).innerText = `${this.transformMonth(month)} ${year}`;
+  }
+}
+
+class CalenderEvent {
+	constructor({ date, rootElem, ui }){
+		this.ui = ui
+		this.date = date
+		this.rootElem = rootElem
+		
+		this.eventHandler()
 	}
+  eventHandler() {
+    this.rootElem.addEventListener("click", (event) => {
+      if (event.target.closest(".calender-body__dayList"))
+        this.dateClickHandler();
+      if (event.target.closest(".calender-prev__btn"))
+        this.prevCalenderHandler();
+      if (event.target.closest(".calender-next__btn"))
+        this.nextCalenderHandler();
+    });
+  }
 
-	render(date){
-		const { dateHTML, month, year } = this.datePrint(date)
-		document.querySelector(`.calender-body__date`).innerHTML = dateHTML
-		document.querySelector(`.calender-header__year span`).innerText = `${this.transformMonth(month)} ${year}`;
-	}
+  dateClickHandler() {
+    this.selected && this.selected.classList.remove("selected");
+    event.target.closest(".calender-body__dayList").classList.add("selected");
+    this.selected = event.target.closest(".calender-body__dayList");
+  }
 
-	eventHandler(){
-		let selected;
+  prevCalenderHandler() {
+    this.ui.render(new Date(this.date.setMonth(this.date.getMonth() - 1)));
+  }
 
-		this.#rootElem.addEventListener('click', (event) => {
-
-			if(event.target.closest('.calender-body__dayList')){
-				selected && selected.classList.remove('selected')
-				event.target.closest('.calender-body__dayList').classList.add('selected')
-				selected = event.target.closest('.calender-body__dayList')
-			}
-
-			if(event.target.closest('.calender-prev__btn')){
-				this.render(new Date(this.#date.setMonth(this.#date.getMonth() - 1)));
-			}
-
-			if(event.target.closest('.calender-next__btn')){
-				this.render(new Date(this.#date.setMonth(this.#date.getMonth() + 1)));
-			}
-
-		})
-	}
+  nextCalenderHandler() {
+    this.ui.render(new Date(this.date.setMonth(this.date.getMonth() + 1)));
+  }
 }
 
 new Calender({
-	date: new Date(),
-	rootElem: document.querySelector('.calender'),
+  date: new Date(),
+  rootElem: document.querySelector(".calender"),
 });
